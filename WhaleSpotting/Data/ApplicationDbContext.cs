d4446dcd2c33d685bc72f205ede7f938ffe4c247
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2023-11-06 11:08:11 +0000

    WS-001: Create migration for Sighting model

--- WhaleSpotting/Data/ApplicationDbContext.cs
+++ WhaleSpotting/Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace WhaleSpotting.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Sighting>? Sightings { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
---
+++ WhaleSpotting/Migrations/20231106110601_AddSightingTable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WhaleSpotting.Migrations
+{
+    public partial class AddSightingTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sightings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Latitude = table.Column<float>(type: "real", nullable: false),
+                    Longitude = table.Column<float>(type: "real", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sightings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sightings");
+        }
+    }
+}
