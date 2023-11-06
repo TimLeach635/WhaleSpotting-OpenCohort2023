e7d2248727d391780b7f11fa388aaa32028fa08c
MoeyCC <mohan.ratnam@cornwall.gov.uk>
2023-11-06 12:14:02 +0000

    WS-001: Create basic sighting controller

---
+++ WhaleSpotting/Controllers/SightingController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WhaleSpotting.Models;
+
+namespace WhaleSpotting.Controllers;
+
+public class SightingController : Controller
+{
+    private readonly ILogger<SightingController> _logger;
+
+    public SightingController(ILogger<SightingController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
