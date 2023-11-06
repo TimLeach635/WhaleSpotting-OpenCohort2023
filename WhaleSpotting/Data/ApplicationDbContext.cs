b603e21bb955410be4b86ce334e517e2e03ef325
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-11-06 12:22:51 +0000

    WS-027: Removed blank line

--- WhaleSpotting/Data/ApplicationDbContext.cs
+++ WhaleSpotting/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace WhaleSpotting.Data;
 public class ApplicationDbContext : IdentityDbContext
 {
     public DbSet<Sighting>? Sightings { get; set; }
-
     public DbSet<Species>? Species { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
