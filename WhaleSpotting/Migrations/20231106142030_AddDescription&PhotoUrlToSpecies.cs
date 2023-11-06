6c78905f6039a66c9bc43b368fcebe2173c6dfd9
Munshi <farhan.munshi@chubbfs.com>
2023-11-06 14:22:40 +0000

    WS-032-AddDescription&PhotoUrlToSpecies

---
+++ WhaleSpotting/Migrations/20231106142030_AddDescription&PhotoUrlToSpecies.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhaleSpotting.Migrations
+{
+    public partial class AddDescriptionPhotoUrlToSpecies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Species",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                table: "Species",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Species");
+
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                table: "Species");
+        }
+    }
+}
--- WhaleSpotting/Models/Species.cs
+++ WhaleSpotting/Models/Species.cs
@@ -4,4 +4,6 @@ public class Species
 {
     public int Id { get; set; }
     public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? PhotoUrl { get; set; }
 }
