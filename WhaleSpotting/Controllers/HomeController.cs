f202b78a53c94043ff6f11c1bbdec730d2aa6164
MTHarkins <michael.harkins@softwire.com>
2023-11-07 14:31:43 +0000

    WS-018: Add homepage endpoint weekly sightings

--- WhaleSpotting/Controllers/HomeController.cs
+++ WhaleSpotting/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using WhaleSpotting.Data;
 using WhaleSpotting.Models;
 
 namespace WhaleSpotting.Controllers;
@@ -7,10 +8,12 @@ namespace WhaleSpotting.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+            _context = context;
     }
 
     public IActionResult Index()
@@ -22,6 +25,24 @@ public class HomeController : Controller
     {
         return View();
     }
+
+        [Route("api/images/lastweek")]
+        [HttpGet]
+        public IActionResult GetImagesFromLastWeek()
+        {
+            var lastweek = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(-7));
+
+            var sightings = _context.Sightings!
+                .Where(sighting => sighting.Date >= lastweek)
+                .ToList();
+
+            if (sightings == null || sightings.Count == 0)
+            {
+                return NotFound("No images found from the last week.");
+            }
+
+            return Ok(sightings);
+        }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
