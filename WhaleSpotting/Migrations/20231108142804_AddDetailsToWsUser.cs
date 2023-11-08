4f9ed2c37534cf3e385d43850d377430398614c1
Tim Leach <4410524+TimLeach635@users.noreply.github.com>
2023-11-08 14:28:57 +0000

    WS-000: Add details to WsUser model

---
+++ WhaleSpotting/Migrations/20231108142804_AddDetailsToWsUser.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhaleSpotting.Migrations
+{
+    public partial class AddDetailsToWsUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Bio",
+                table: "WsUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "WsUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "WsUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Bio",
+                table: "WsUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "WsUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "WsUsers");
+        }
+    }
+}
