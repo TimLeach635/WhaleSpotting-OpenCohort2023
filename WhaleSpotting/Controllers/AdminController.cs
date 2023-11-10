5fdece4c37269784cf4bd27efb03566699c9addd
MTHarkins <michael.harkins@softwire.com>
2023-11-10 11:02:09 +0000

    Format indentation and add foreach

--- WhaleSpotting/Controllers/AdminController.cs
+++ WhaleSpotting/Controllers/AdminController.cs
@@ -25,23 +25,23 @@ public class AdminController : Controller
 
         return View(unapprovedSightings);
     }
-     [HttpPost]
-        public IActionResult ApproveSightings(List<int> selectedSightingIds)
+    [HttpPost]
+    public IActionResult ApproveSightings(List<int> selectedSightingIds)
+    {
+        if (selectedSightingIds != null && selectedSightingIds.Any())
         {
-            if (selectedSightingIds != null && selectedSightingIds.Any())
+            var selectedSightings = _context.Sightings!
+                .Where(s => selectedSightingIds.Contains(s.Id))
+                .ToList();
+
+            foreach (var sighting in selectedSightings)
             {
-                var selectedSightings = _context.Sightings!
-                    .Where(s => selectedSightingIds.Contains(s.Id))
-                    .ToList();
-
-                foreach (var sighting in selectedSightings)
-                {
-                    sighting.Approved = true;
-                }
-
-                _context.SaveChanges();
+                sighting.Approved = true;
             }
 
-            return RedirectToAction("Index");
+            _context.SaveChanges();
         }
+
+        return RedirectToAction("Index");
+    }
 }
