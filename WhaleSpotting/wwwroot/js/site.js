ae9614b68a27a531cb9e1fd834080c71fce3d08f
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-11-15 14:35:14 +0000

    working carousel with fixed imgs

--- WhaleSpotting/wwwroot/js/site.js
+++ WhaleSpotting/wwwroot/js/site.js
@@ -1,4 +1,38 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+let currentIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const dots = document.querySelectorAll('.dot');
+ 
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+    });
+    dots.forEach((dot) => {
+      dot.classList.remove('active');
+    });
+ 
+    slides[index].classList.add('active');
+    dots[index].classList.add('active');
+  }
+ 
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+  }
+ 
+  function previousSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+  }
+ 
+ 
+  setInterval(nextSlide, 5000);
+ 
+ 
+  dots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      currentIndex = index;
+      showSlide(currentIndex);
+    });
+  });
