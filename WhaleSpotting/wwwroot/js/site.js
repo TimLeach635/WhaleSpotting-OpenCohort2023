2d0a73636451e926a85857dbf74d16cb0e7de675
meganrdp <100476117+meganrdp@users.noreply.github.com>
2023-11-16 10:44:33 +0000

    Made requested changes

--- WhaleSpotting/wwwroot/js/site.js
+++ WhaleSpotting/wwwroot/js/site.js
@@ -1,31 +1,28 @@
-﻿
-
-let currentIndex = 0;
-  const slides = document.querySelectorAll('.slide');
-   
-  function showSlide(index) {
-    slides.forEach((slide) => {
-      slide.classList.remove('active');
-    });
-    slides[index].classList.add('active');
-  }
- 
-  function firstSlide() {
-    currentIndex = 0;
-    showSlide(currentIndex);
-  }
-
-  function nextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
-  }
- 
-  function previousSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-    showSlide(currentIndex);
-  }
- 
-  firstSlide();
- 
-  setInterval(nextSlide, 5000);
+﻿let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
   
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+  slides[index].classList.add('active');
+}
+
+function firstSlide() {
+  currentIndex = 0;
+  showSlide(currentIndex);
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+function previousSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+}
+
+firstSlide();
+
+setInterval(nextSlide, 5000);
